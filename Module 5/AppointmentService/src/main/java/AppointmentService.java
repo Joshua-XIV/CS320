e21ce783f1c0aba4ca2d421945977f1239fc1b4a
Joshua <josh.hdz956@yahoo.com>
2026-03-04 23:36:21 -0600

    appointment service, add and delete

--- Module 5/AppointmentService/src/main/java/AppointmentService.java
+++ Module 5/AppointmentService/src/main/java/AppointmentService.java
@@ -1,2 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppointmentService {
+    private final Map<String, Appointment> appointments = new HashMap<>();
+
+    public void addAppointment(Appointment appointment) {
+        if (appointment == null) {
+            throw new IllegalArgumentException("Appointment object can not be null");
+        }
+
+        if (appointments.containsKey(appointment.getAppointmentID())) {
+            throw new IllegalArgumentException("Appointment ID already exist");
+        }
+
+        appointments.put(appointment.getAppointmentID(), appointment);
+    }
+
+    public void deleteAppointment(String appointmentID) {
+        if (appointmentID == null) {
+            throw new IllegalArgumentException("Appointment ID can not be null");
+        }
+
+        if (!appointments.containsKey(appointmentID)) {
+            throw new IllegalArgumentException("Appointment ID does not exist");
+        }
+
+        appointments.remove(appointmentID);
+    }
 }
